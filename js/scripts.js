9e4f656a7fc7fd4fd582e75711839d22b18b3445
taylorsutton <tsutton@engr-131-14.eastern.ewu.edu>
2016-05-23 16:44:29 -0700

    weather changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,46 @@
+// First, load jQuery (required)
+// jQuery 2.2.2 loaded via `index.html`
+
+// Second, load Semantic UI JS (required)
+// @codekit-prepend "semantic.js";
+
+// Third, Add to Homescreen (optional)
+// @codekit-prepend "add-to-homescreen.js";
+
+// Third, load any plugins (optional)
+// @codekit-prepend "plugins.js";
+
 // My Scripts
 
-// Simple Weather javascript
 
-$(function() {
-  $.simpleWeather({
-    location: 'Spokane, MA',
-    
+
+//Activate Tabs
+$('.menu .item').tab()
+;
+
+//Get Cheney Weather
+// Note: Plugin has been updated. Use version 3.1
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+// Refer: http://simpleweatherjs.com
+
+$.simpleWeather({
+    location: '99004',
+    woeid: '',
     unit: 'f',
-    
     success: function(weather) {
-        $('.city').text(weather.city);
-        $('.temp').text(weather.temp);
-        $('.cond-code').text(weather.code);
-        console.log(weather);
-        $('.icon img').attr('src', weather.image);
-        
-      },
-
+      
+      // Display Data
+      $('#cheney .city').text(weather.temp);
+      $('#cheney .temp').text(weather.city);
+      $('#cheney i').addClass( 'icon-' + weather.code );
+    
+      // Entire weather object
+      console.log(weather);
+    },
     error: function(error) {
-      console.log("ERROR NO WEATHER!");
+      // Show if weather cannot be retreived
     }
+  
   });
-});
 
-// -----------------------
+
